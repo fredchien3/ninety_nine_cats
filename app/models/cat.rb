9446a7f42fcda5412557d17da3c833881da40ce8
Frederick <fred.chien3@gmail.com>
2022-02-20 15:06:48 +0800

    initial commit with cat migration and model

---
+++ app/models/cat.rb
@@ -0,0 +1,24 @@
+require 'action_view'
+
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+  COLORS = [
+
+  validates_presence_of :name, :color, :sex, :birth_date, :description
+  validates :color, inclusion: { in: %w(white black brown calico), message: "%{value} is not a valid color" }
+  validates :sex, length: { is: 1 }, inclusion: { in: %w(M F), message: "%{value} is not a valid sex" }
+
+  def age
+    today = Date.today
+    birthday = self.birth_date
+
+    days_old = (Date.today - self.birth_date).to_i
+    return "#{days_old} days" if days_old <= 30
+
+    months_old = days_old.days / 1.months
+    return "#{months_old} months" if months_old <= 11
+
+    years_old = days_old.days / 1.years
+    return "#{years_old} years"
+  end
+end
